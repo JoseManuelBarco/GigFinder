f59f648422bad7767c8603e0fdefcba2a86746eb
CEP-TARDA <jbarcoc2324@politecnics.tarda>
2025-02-14 20:19:18 +0100

    Hecho ambos layouts

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,36 +1,28 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_activity)
 
-        val txtNom: EditText = findViewById(R.id.TxtName)
-        val btnOk: Button = findViewById(R.id.BtnOk)
-        val lblNom: TextView = findViewById(R.id.LblNom)
+        val signupButton: Button = findViewById(R.id.loginButton)
+        val loginTextView: TextView = findViewById(R.id.registerTextView)
 
-        btnOk.setOnClickListener(){
-            var text = txtNom.text.toString()
-            if (text.equals("")){
-                lblNom.text = "El teu nom és " + text
-            }
-            else
-            {
-                lblNom.text = ""
-                Toast.makeText(this, "No has introduit el teu nom",Toast.LENGTH_LONG).show()
-            }
+        signupButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity1::class.java)
+            startActivity(intent)
+        }
+
+        loginTextView.setOnClickListener {
+            val intent = Intent(this, RegisterActivity1::class.java)
+            startActivity(intent)
         }
     }
 }

7e5028faed9aedcbd8a4e71308d83707a1b7df3a
Desirée Romero <dromerol2122@politecnics.barcelona>
2025-02-18 19:59:04 +0100

    Commit test

--- app/src/main/java/com/example/myapplication/RegisterActivity1.kt
+++ app/src/main/java/com/example/myapplication/RegisterActivity1.kt
@@ -7,6 +7,5 @@ class RegisterActivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_activity)
-
     }
 }
